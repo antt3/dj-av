3c6fb94fe7f940aac4a218c9f26d5a942b3b33c9
antt3 <anthonyt3@sbcglobal.net>
2025-06-23 19:50:07 -0700

    Updated Default Config Variables & Test Ran Sucessfully

--- config.ts
+++ config.ts
@@ -1,31 +1,33 @@
 import type { AvailabilitySlotsMap } from "./lib/types"
 
-export const ALLOWED_DURATIONS = [15, 30, 60]
+export const ALLOWED_DURATIONS = [...Array(6).keys()].map((v) => (v + 1) * 15);
 
 export const DEFAULT_DURATION = 30
 
 export const CALENDARS_TO_CHECK = ["primary"]
-export const SLOT_PADDING = 0
+export const SLOT_PADDING = 15
 export const OWNER_TIMEZONE = "America/Los_Angeles"
 export const LEAD_TIME = 0
 
 const DEFAULT_WORKDAY = [
   {
     start: {
-      hour: 9,
+      hour: 0,
     },
     end: {
-      hour: 22,
+      hour: 24,
     },
   },
 ]
 
 export const OWNER_AVAILABILITY: AvailabilitySlotsMap = {
+  0: DEFAULT_WORKDAY,
   1: DEFAULT_WORKDAY,
   2: DEFAULT_WORKDAY,
   3: DEFAULT_WORKDAY,
   4: DEFAULT_WORKDAY,
   5: DEFAULT_WORKDAY,
+  6: DEFAULT_WORKDAY,
 }
 
 export const LOCAL_DATE_OPTIONS: Intl.DateTimeFormatOptions = {
